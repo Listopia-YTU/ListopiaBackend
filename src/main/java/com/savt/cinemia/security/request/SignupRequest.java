a40c6437564a2f0bdf4415f2ddf7aa9b6cd6d271
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-22 19:34:06 +0300

    feat: add LoginRequest.java and SignupRequest.java to package: 'security.request'

---
+++ src/main/java/com/savt/cinemia/security/request/SignupRequest.java
@@ -0,0 +1,26 @@
+package com.savt.cinemia.security.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class SignupRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    private Set<String> role;
+
+}
