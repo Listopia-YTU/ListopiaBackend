c8ccc24b18944f83fe117aacb3534eadc24f05c4
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2025-04-22 19:39:58 +0300

    cors: added listopia.pages.dev domain

--- src/main/java/com/savt/listopia/config/WebConfiguration.java
+++ src/main/java/com/savt/listopia/config/WebConfiguration.java
@@ -10,7 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://*.ensargok.com", "https://*.cinemium.net", "http://localhost:5173/", "http://127.0.0.1:5173/")
+                .allowedOriginPatterns("https://*.ensargok.com", "https://*.cinemium.net", "http://localhost:5173/", "http://127.0.0.1:5173/", "https://*.listopia.pages.dev/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
