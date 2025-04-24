55ffd8dae6bbebe52f5b6f94d8b525b18ff2cf82
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2025-04-24 16:52:03 +0300

    cors: updated to add localhost

--- src/main/java/com/savt/listopia/config/WebConfiguration.java
+++ src/main/java/com/savt/listopia/config/WebConfiguration.java
@@ -10,7 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://ensargok.com", "https://listopia.pages.dev", "http://127.0.0.1:5173")
+                .allowedOrigins("https://ensargok.com", "https://listopia.pages.dev", "http://127.0.0.1:5173", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowedHeaders("*")
                 .allowCredentials(true);
