24c3627a020f9f0f9df0fa017b32d21e8b1f5a94
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2025-05-01 10:33:10 +0300

    chore: mail api

--- src/main/java/com/savt/listopia/util/MailUtil.java
+++ src/main/java/com/savt/listopia/util/MailUtil.java
@@ -1,7 +1,15 @@
 package com.savt.listopia.util;
 
+import com.savt.listopia.util.mail.MailSender;
+
 public class MailUtil {
-    public static void sendMail(String to, String subject, String text) {
-        // TODO: implement :)
+    public static void sendMail(
+            String receiverMail,
+            String receiverName,
+            String textPart,
+            String htmlPart,
+            String subject
+    ) {
+        MailSender.sendMailFromListopia(receiverMail, receiverName, textPart, htmlPart, subject);
     }
 }
